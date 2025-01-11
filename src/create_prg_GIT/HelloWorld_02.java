9a88612d9fa59288aa73b4cc797fda0717ea4055
MrunalSolanki <dhavalemrunal.1998@gmail.com>
2025-01-11 11:13:17 +0530

    tableof9_11thJan

--- src/create_prg_GIT/HelloWorld_02.java
+++ src/create_prg_GIT/HelloWorld_02.java
@@ -1,6 +1,8 @@
+package create_prg_GIT;
+
 public class HelloWorld_02 {
 
     public static void main(String[] args) {
-        System.out.println("Hi I  am Mrunal");
+        System.out.println("Hi I v am Mrunal");
     }
 }
