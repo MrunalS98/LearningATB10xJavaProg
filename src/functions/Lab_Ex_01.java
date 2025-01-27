6f483f0978475ce8e57ee7aa9d9260493e8b98c6
MrunalSolanki <dhavalemrunal.1998@gmail.com>
2025-01-27 16:39:54 +0530

    function Ex1

---
+++ src/functions/Lab_Ex_01.java
@@ -0,0 +1,20 @@
+package functions;
+
+public class Lab_Ex_01 {
+    public static void main(String[] args) {
+
+        greetOne();
+        //System.out.println();
+        greet();
+        //System.out.println();
+    }
+        static void greetOne(){                                     // function Declaration
+            System.out.println("Hi, I am function One");
+        }
+
+        static void greet(){
+            System.out.println("Hi, I am function Two");
+        }
+
+    }
+
---
+++ src/functions/User_define_functions.java
@@ -0,0 +1,41 @@
+package functions;
+
+public class User_define_functions {
+    public static void main(String[] args) {
+
+        func_one();
+
+        String s = func_two();
+        System.out.println(s);
+
+        func_three("Hey", "  Hello");
+        func_three("Hi", "  how are you");
+
+        int sum = sum_of_numbers(5,4);
+        System.out.println(sum);
+        int sum2 = sum_of_numbers(9,4);
+        System.out.println(sum2);
+        int sum3 = sum_of_numbers(5,7);
+        System.out.println(sum3);
+    }
+
+    //1.Without Parameters and Without Return Type
+    static void func_one(){
+        System.out.println("Hi I am Without Parameters and Without Return Type function");
+    }
+
+    //2. Without Parameters but With Return Type
+    static String func_two(){
+        return "Hi I am Without Parameters but With Return Type function";
+    }
+
+    //With Parameters and Without Return Type ( 90%)
+    static void func_three(String firstname, String lastname){
+        System.out.println("Your name is -> " +firstname +lastname );
+    }
+
+    //4. With Parameters and With Return Type
+    static int sum_of_numbers(int a, int b){
+        return a+b;
+    }
+}
