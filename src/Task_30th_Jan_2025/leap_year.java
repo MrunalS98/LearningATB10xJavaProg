5d15aa3e0fe7589b9a822fd252161911c331e4cf
MrunalSolanki <dhavalemrunal.1998@gmail.com>
2025-01-30 17:42:06 +0530

    leap year corrections

--- src/Task_30th_Jan_2025/leap_year.java
+++ src/Task_30th_Jan_2025/leap_year.java
@@ -11,10 +11,8 @@ public class leap_year {
         System.out.println("Enter Year:-");
         int year = sc.nextInt();
 
-        if(year%4==0 && year%100==0){
-            if (year%400==0){
+        if(year%400==0 || (year%100 !=0 && year %4==0)){
                 System.out.println("Year is leap year");
-            }
         }
         else {
             System.out.println("Year is not a leap year");
